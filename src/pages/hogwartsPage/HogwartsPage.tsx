54af632ab01ff7b26ed428b7147fc97f28042f96
Ann <socevilaf@gmail.com>
2025-03-26 12:02:20 +0300

    Fix Link

--- src/pages/hogwartsPage/HogwartsPage.tsx
+++ src/pages/hogwartsPage/HogwartsPage.tsx
@@ -2,8 +2,8 @@ import Slider from "@components/ui/Slider";
 import { FC, useState } from "react";
 import classes from "./HogwartsPage.module.scss";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { RootState } from "@store/store";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "@store/store";
 import HouseCard from "@components/HouseCard";
 import HogwartsBanner from "@components/HogwartsBanner";
 import { hogwartsImagesData } from "@store/hogwartsImageData";
@@ -12,10 +12,14 @@ import LetterModal from "@components/LetterModal/LetterModal";
 import three from "@assets/three.png";
 import { Tooltip } from "antd";
 import { chooseStudentById } from "@features/studentsSlice";
+import React from "react";
 
 const HogwartsPage: FC = () => {
   const houses = useSelector((state: RootState) => state.houses.houses);
-  const students = useSelector((state: RootState) => state.students.allStudents);
+  const students = useSelector(
+    (state: RootState) => state.students.allStudents
+  );
+  const dispatch = useDispatch<AppDispatch>();
   const hogwartsImages = hogwartsImagesData;
 
   const navigate = useNavigate();
@@ -26,15 +30,15 @@ const HogwartsPage: FC = () => {
     navigate(`/house/${houseName}`);
   };
   const handleImageClick = () => seIsShowModal(!isShowModal);
-  const handleCloseClick = () => seIsShowModal(false);  
+  const handleCloseClick = () => seIsShowModal(false);
   const handleGoClick = () => navigate("/create-student");
   const handleMouseEnter = () => seIsShowTooltip(true);
   const handleMouseLeave = () => seIsShowTooltip(false);
 
-    const handleLinkClick = (studentId: string) => {
-      navigate(`/students/${studentId}`);
-      dispatch(chooseStudentById(studentId));
-    };
+  const handleLinkClick = (studentId: string) => {
+    navigate(`/students/${studentId}`);
+    dispatch(chooseStudentById(studentId));
+  };
 
   return (
     <div className={classes.wrapperHgPageSlider}>
@@ -47,22 +51,20 @@ const HogwartsPage: FC = () => {
 
       {/* TODO: вынести div контейнер в ui компонет, который задает высоту и позиционирование или просто общий класс для дивов ⬇*/}
       <div className={classes.hgPageTitle}>
-
         <Tooltip
-        title="I have a letter for you!"
-        open={isShowTooltip}
-        placement="right"
-   
-      >
-              {/* TODO: вынести div контейнер в ui компонет, который задает высоту и позиционирование или просто общий класс для дивов ⬇*/}
-        <img
-          src={owl}
-          className={classes.hgPageSliderImg}
-          onClick={handleImageClick}
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        />
-      </Tooltip>
+          title="I have a letter for you!"
+          open={isShowTooltip}
+          placement="right"
+        >
+          {/* TODO: вынести div контейнер в ui компонет, который задает высоту и позиционирование или просто общий класс для дивов ⬇*/}
+          <img
+            src={owl}
+            className={classes.hgPageSliderImg}
+            onClick={handleImageClick}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          />
+        </Tooltip>
         <h1 className={classes.titleMain}>Welcome to Hogwarts</h1>
       </div>
 
@@ -88,36 +90,44 @@ const HogwartsPage: FC = () => {
 
       {/* TODO: вынести div контейнер в ui компонет который задает высоту и позиционирование или просто общий класс для дивов ⬇*/}
       <div className={classes.hgPageThree}>
-
-        {/* TODO: оставить так или вынести как HogwartsBanner ⬆*/}
+        {/* TODO: оставить содержимое блока так или вынести как HogwartsBanner  в компонент ⬆*/}
         <div className={classes.hgPageThreeBg}>
           <div className={classes.hgPageThreeColumn}>
             <h2 className={classes.title}>The famous three</h2>
+
+            {/*TODO: разобрать, вынести в компонент,утилиты  */}
             <p>
-              {/*TODO: править ссылки - React.Fragment? */}
-      {["Harry Potter", "Ron Weasley", "Hermione Granger"].map((name) => {
-        const student = students.find((s) => s.name === name);
-        return student ? (
-          <a
-            key={student.id}
-            href={`/students/${student.id}`}
-            onClick={(e) => {
-              e.preventDefault();
-              handleLinkClick(student.id);
-            }}
-          >
-            {name}
-          </a>
-        ) : (
-          name
-        );
-      })}
-      {" "}are students at Hogwarts School of Witchcraft and Wizardry. Together, they faced many challenges and
-      defeated a terrifying monster that threatened the school. With
-      Harry’s bravery, Ron’s loyalty, and Hermione’s cleverness, they
-      proved that true friendship and courage can overcome even the
-      darkest dangers.
-    </p>
+              {["Harry", "Ron", "Hermione"].map((firstName, index) => {
+                const student = students.find((s) =>
+                  s.name.startsWith(firstName)
+                );
+
+                return student ? (
+                  <React.Fragment key={student.id}>
+                    {index > 0 && " "}
+                    <a
+                      href={`/students/${student.id}`}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handleLinkClick(student.id);
+                      }}
+                    >
+                      {firstName}
+                    </a>
+                  </React.Fragment>
+                ) : (
+                  <React.Fragment key={firstName}>
+                    {index > 0 && " "}
+                    {firstName}
+                  </React.Fragment>
+                );
+              })}{" "}
+              are students at Hogwarts School of Witchcraft and Wizardry.
+              Together, they faced many challenges and defeated a terrifying
+              monster that threatened the school. With Harry’s bravery, Ron’s
+              loyalty, and Hermione’s cleverness, they proved that true
+              friendship and courage can overcome even the darkest dangers.
+            </p>
           </div>
 
           <div className={classes.hgPageThreeImage}>
@@ -130,7 +140,3 @@ const HogwartsPage: FC = () => {
 };
 
 export default HogwartsPage;
-function dispatch(arg0: any) {
-  throw new Error("Function not implemented.");
-}
-
