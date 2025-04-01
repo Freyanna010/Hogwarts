cc10a2b4ae694ce551b7cb069ce9529f7de4023e
Ann <socevilaf@gmail.com>
2025-04-01 15:04:05 +0300

    Add HighlightedLinks

--- src/components/ui/HighlightedLinks/HighlightedLinks.tsx
+++ src/components/ui/HighlightedLinks/HighlightedLinks.tsx
@@ -1,11 +1,39 @@
+import React from "react";
 import { HighlightedLinksProps } from "./HighlightedLinks.types";
 
 const HighlightedLinks = <T extends { id: string; name: string }>(
-  props: HighlightedLinksProps<T>,
+  props: HighlightedLinksProps<T>
 ) => {
-  const { linkWords, linkItems, className, onClick } = props;
+const { linkWords, linkItems, className, onClick } = props;
 
-  return <div></div>;
+  return (
+    <>
+    {linkWords.map((word, index) => {
+        const item = linkItems.find((el) => el.name.startsWith(word));
+
+        return item ? (
+        <React.Fragment key={item.id}>
+            {index > 0 && " "}
+            <a
+            className={className}
+            href={`#${item.id}`}
+            onClick={(e) => {
+                e.preventDefault();
+                onClick(item.id);
+            }}
+            >
+            {word}
+            </a>
+        </React.Fragment>
+        ) : (
+        <React.Fragment key={word}>
+            {index > 0 && " "}
+            {word}
+        </React.Fragment>
+        );
+    })}
+    </>
+);
 };
 
 export default HighlightedLinks;
