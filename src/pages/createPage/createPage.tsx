48fe1a1f1e442115b43b29f9b4320dbe4b3e4d69
Ann <socevilaf@gmail.com>
2025-04-16 11:29:57 +0300

    Add todu in   CreatePage

--- src/pages/createPage/createPage.tsx
+++ src/pages/createPage/createPage.tsx
@@ -1,6 +1,9 @@
 import StudentForm from "@components/StudentForm";
 
 const CreatePage = () => {
+  // TODO: Поднять компонент
+  // TODO: Стиль кнопки
+  // TODO:  FEATURE: тест на выбор хауса
   return <StudentForm />;
 };
 
