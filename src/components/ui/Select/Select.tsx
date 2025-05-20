5f2d83b8a596c14b5c7135441aeebd98f6f40ff6
Ann <socevilaf@gmail.com>
2025-05-20 14:07:26 +0300

    Fix Select

--- src/components/ui/Select/Select.tsx
+++ src/components/ui/Select/Select.tsx
@@ -2,50 +2,46 @@ import { useFormContext } from "@shared/form";
 import classes from "./Select.module.scss";
 import { SelectProps } from "./Select.types";
 
-const Select = <T, K extends Extract<keyof T, string>>(props: SelectProps<T, K>) => {
-      const {
+const Select = <T, K extends Extract<keyof T, string>>(
+  props: SelectProps<T, K>
+) => {
+  const {
     name,
     selected,
     options,
     placeholder = "Choose...",
-    onChange,
     isRequired = false,
     errorMessage,
     label,
   } = props;
 
-    const { formData, setFormValue, } =
-      useFormContext<T>();
+  const { formData, setFormValue } = useFormContext<T>();
 
-      const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) =>{
-        const newValue = e.target.value as T[K]
-setFormValue(name, newValue )
-      }
-
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newValue = e.target.value as T[K];
+    setFormValue(name, newValue);
+  };
 
   return (
     <div>
       <div className={classes.labelRow}>
         <p>*</p>
-{/* TODO: вынести в компонент */} 
+        {/* TODO: вынести в компонент */}
         <label className={classes.label}>{label}</label>
       </div>
 
       <select value={formData[name] as string} onChange={handleChange}>
         <option value="">{placeholder}</option>
-   {options?.map(option=>(
-    <option value={option.value}>{option.title}</option>
-   ))}
+        {options?.map((option) => (
+          <option value={option.value}>{option.title}</option>
+        ))}
       </select>
 
-{/* TODO: вынести в компонент */} 
+      {/* TODO: вынести в компонент */}
       {isRequired && !selected && (
         <p className={classes.error}>{errorMessage}</p>
       )}
-      
     </div>
-
-    
   );
 };
 
