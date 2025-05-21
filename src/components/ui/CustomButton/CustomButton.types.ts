23c53331e6a4c5219f93d1580eb14774a989ab16
Ann <socevilaf@gmail.com>
2025-05-21 11:20:28 +0300

    Fix CustomButton

--- src/components/ui/CustomButton/CustomButton.types.ts
+++ src/components/ui/CustomButton/CustomButton.types.ts
@@ -1,5 +1,5 @@
 
-export type ButtonType = "primary" | "default" | "ghost" | "danger" | "link" | "icon";
+export type ButtonType = "primary" | "default" | "danger" | "link" | "icon";
 export type ButtonSize = "large" | "middle" | "small";
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
