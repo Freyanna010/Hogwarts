b24ac6111c3c2d5c9c0e929443bb2a823cddb479
Ann <socevilaf@gmail.com>
2025-08-19 16:14:06 +0300

    Fix scroll

--- src/shared/hooks/useScrollLock.ts
+++ src/shared/hooks/useScrollLock.ts
@@ -2,14 +2,14 @@ import { useEffect } from "react";
 
 export const useScrollLock = (isOpen: boolean) => {
   useEffect(() => {
-    if (!isOpen) {
-      return;
-    }
+    if (!isOpen) return;
+
+    const originalScrollStyle = window.getComputedStyle(document.body).overflow;
+
     document.body.style.overflow = "hidden";
 
     return () => {
-    document.body.style.overflow = "auto";
-    }
-
+      document.body.style.overflow = originalScrollStyle;
+    };
   }, [isOpen]);
 };
